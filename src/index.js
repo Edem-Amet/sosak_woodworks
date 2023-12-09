6cb6bab5c9fe7c18d1e6bca94122a5d78ece81eb
Amet Edem Emmanuel <143708044+Edem-Amet@users.noreply.github.com>
2023-12-09 13:54:11 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,54 +10,25 @@ import Products from './components/products';
 import Contact from './components/contact';
 
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from "react-router-dom";
-
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-
-  {
-    path: "/src/components/services.js",
-    element: <Services />,
-  },
-
-  {
-    path: "/src/components/about.js",
-    element: <About />,
-  },
-
-  {
-    path: "/src/components/contact.js",
-    element: <Contact />,
-  },
-
-  {
-    path: "/src/components/home.js",
-    element: <Home />,
-  },
-
-  {
-    path: "/src/components/products.js",
-    element: <Products />,
-  },
-
-
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router basename="/sosak_woodworks">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
